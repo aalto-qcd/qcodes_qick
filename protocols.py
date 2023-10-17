51122366b91e27c35f360b3b11b282d5544a7664
Jonatan Albanese <jonatan.albanese@aalto.fi>
2023-10-17 11:04:24 +0300

    Did some commenting

--- protocols.py
+++ protocols.py
@@ -10,15 +10,25 @@ class Protocol:
         pass
             
     def initialize_program(self):
-        """ can be used for input validation? """
+        """ 
+        Abstract method for possible initialization functionality 
+
+        """
         pass
 
     def run_program(self):
-        """ handle running the program and aqcuiring the data """
+        """ 
+        Abstract method for running the program and returning th
+        measurement result in the correct form.
+
+        """
         pass
 
     def handle_output(self):
-        """ output handling? """
+        """ 
+        Abstract method for handling the output into the correct form 
+
+        """
         pass
 
 
@@ -26,14 +36,36 @@ class Protocol:
 class NDSweepProtocol(Protocol):
 
     def __init__(self):
+        """
+        Initialize the protocol object.
+            
+        """
         super().__init__()
+        this.name = "NDSweepMeasurement"
+        this.measurement_type = "IQ_measurement"
 
     def initialize_program(self, soccfg, cfg):
-        """ can be used for input validation? """
+        """ 
+        Initialize the qick soc and qick config
+        
+        """
         self.cfg = cfg
         self.soccfg = soccfg
 
     def run_program(self):
+        """
+        This method runs the program and returns the measurement 
+        result.
+
+        Return:
+            expt_pts:
+            list of arrays containing the coordinate values of 
+            each variable for each measurement point
+            avg_q:
+            ND-array of avg_q values containing each measurement q value.
+            avg_i:
+            ND-array of avg_i values containing each measurement i value.
+        """
         sweep_config = self.cfg["sweep_variables"]
         if "freq" in sweep_config:
             freqs = np.linspace(sweep_config["freq"][0], sweep_config["freq"][1], sweep_config["freq"][2])
@@ -45,24 +77,54 @@ class NDSweepProtocol(Protocol):
             for freq in freqs:
                 self.cfg["pulse_freq"] = freq
                 prog = HardwareSweepProgram(self.soccfg, self.cfg)
-                expt_pts, avg_q, avg_i = prog.acquire(self.soccfg, load_pulses=True)
-                q_data.append(avg_q) 
+                expt_pts, avg_i, avg_q = prog.acquire(self.soccfg, load_pulses=True)
                 i_data.append(avg_i) 
+                q_data.append(avg_q) 
 
-            q_data = np.concatenate( q_data, axis=0 )
             i_data = np.concatenate( i_data, axis=0 )
+            q_data = np.concatenate( q_data, axis=0 )
             expt_pts.append(freqs)
-            return self.handle_output(expt_pts, q_data, i_data)
+            return self.handle_output(expt_pts, i_data, q_data)
                 
         
         else:
             prog = HardwareSweepProgram(self.soccfg, self.cfg)
-            expt_pts, avg_q, avg_i = prog.acquire(self.soccfg, load_pulses=True)
-            expt_pts, avg_q, avg_i = self.handle_output(expt_pts, avg_q, avg_i)
-            return expt_pts, avg_q, avg_i 
+            expt_pts, avg_i, avg_q = prog.acquire(self.soccfg, load_pulses=True)
+            expt_pts, avg_i, avg_q = self.handle_output(expt_pts, avg_i, avg_q)
+            return expt_pts, avg_i, avg_q 
 
 
     def handle_output(self, expt_pts, avg_i, avg_q):
+        """
+        This method handles formatting the output into a standardized
+        form, to be sent to back to the ZCU216Station. 
+
+        Parameters:
+            expt_pts:
+                array of arrays containing each of the experiment
+                values (only once) for each of the sweepable variables.
+                This contains the coordinates of our measurement.
+            avg_i: 
+                I values of the measurement each corresponding to 
+                a specific combination of coordinates. 
+            avg_q: 
+                Q values of the measurement each corresponding to 
+                a specific combination of coordinates. 
+
+
+        Returns:
+            expt_pts: 
+                list of N (coordinate amount) arrays whose each element
+                corresponds to an individual measurement. Thus, the lists
+                will be the same size as there are total measurement points
+                and for each index you may find the corresponding coordinate
+                point of an individual measurement from each of the arrays.
+            avg_i: 
+                In this method, the average i values are unchanged
+            avg_q: 
+                In this method, the average q values are unchanged
+            
+        """
         datapoints = len(avg_i.flatten())
         new_expt_pts = [[] for i in range(len(expt_pts))]
 
@@ -74,5 +136,5 @@ class NDSweepProtocol(Protocol):
                 coord_index += 1
 
 
-        return new_expt_pts, avg_q, avg_i 
+        return new_expt_pts, avg_i, avg_q 
 
