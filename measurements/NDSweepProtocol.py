a1ff5d22100e87136c5976d2f3acbc5e32dd37c2
Jonatan <jonatan.albanese@aalto.fi>
2023-10-19 11:32:22 +0000

    Included NDSweeping into the new refactor, needs testing

--- measurements/NDSweepProtocol.py
+++ measurements/NDSweepProtocol.py
@@ -10,21 +10,21 @@ from typing import List, Dict, Any
 
 
 
-class PulseProbeSpectroscopyProtocol(Protocol):
+class NDSweepProtocol(Protocol):
     """
         This protocol initializes and runs a pulse probe spectroscopy measurement 
         program, and correctly formats the output into a desired form. 
     """
-    def __init__(self, name="PPS_Protocol"):
+    def __init__(self, name="NDS_Protocol"):
         """
         Initialize the protocol object.
             
         """
         super().__init__(name)
 
-        self.required_DACs = {'qubit': 'Qubit probe channel', 'readout' : 'Readout pulse channel', }
+        self.required_DACs = {'probe': 'Probe channel' }
         self.required_ADCs = {'adc' : 'Readout adc channel' }
-        self.validated_IO = {'qubit' : None, 'readout': None, 'adc': None}
+        self.validated_IO = {'probe': None, 'adc': None}
 
         self.sensible_defaults = {
                                    "adc_trig_offset"  : 100,    # -- Clock ticks
@@ -94,31 +94,22 @@ class PulseProbeSpectroscopyProtocol(Protocol):
                     'reps' : self.reps,
                     'relax_delay' : self.relax_delay,
                     'adc_trig_offset' : self.adc_trig_offset,
-
-                    #For the PulseProbeSpectroscopy program, readout probe pulse
-                    #lenght is a good readout lenght
-                    'readout_length' : self.validated_IO['readout'].pulse_length,
+                    'readout_length' : self.readout_length,
 
                     #These are channel specific values 
-                    'qubit_ch' : self.validated_IO['qubit'].channel,
-                    'cavity_ch' : self.validated_IO['readout'].channel,
+                    'probe_ch' : self.validated_IO['probe'].channel,
                     'ro_ch' : self.validated_IO['adc'].channel,
-                    'qubit_nqz' : self.validated_IO['qubit'].nqz,
-                    'cavity_nqz' : self.validated_IO['readout'].nqz,
-                    'qubit_gain' : self.validated_IO['qubit'].pulse_gain,
-                    'cavity_gain' : self.validated_IO['readout'].pulse_gain,
-                    'cavity_freq' : self.validated_IO['readout'].pulse_freq,
-                    'qubit_phase' : self.validated_IO['qubit'].pulse_phase,
-                    'cavity_phase' : self.validated_IO['readout'].pulse_phase,
-                    'qubit_length' : self.validated_IO['qubit'].pulse_length,
-                    'cavity_length' : self.validated_IO['readout'].pulse_length
+                    'probe_nqz' : self.validated_IO['probe'].nqz,
                    }
 
         external_config = self.compile_software_sweep_dict( sweep_configuration, external_parameters )
 
         # Internal parameters that can be swept in hardware
         internal_parameters = { 
-                    'qubit_freq' : self.validated_IO['qubit'].pulse_freq,
+                    'qubit_freq' : self.validated_IO['probe'].pulse_freq,
+                    'probe_gain' : self.validated_IO['probe'].pulse_gain,
+                    'probe_phase' : self.validated_IO['probe'].pulse_phase,
+                    'probe_length' : self.validated_IO['probe'].pulse_length,
                    }
 
         internal_config = self.compile_hardware_sweep_dict(sweep_configuration, internal_parameters)
@@ -151,62 +142,77 @@ class PulseProbeSpectroscopyProtocol(Protocol):
         expt_pts, avg_i, avg_q = self.run_hybrid_loop_program(self.cfg, PulseProbeSpectroscopyProgram, iterators)
         return expt_pts, avg_i, avg_q 
 
-class PulseProbeSpectroscopyProgram(RAveragerProgram):
 
+class HardwareSweepProgram(NDAveragerProgram):
+    """
+    This class performs a hardware loop sweep over one or more registers 
+    in the board. The limit is seven registers.
+
+
+    Methods
+    -------
+    initialize(self):
+        Initializes the program and defines important variables and registers.
+        The sweeps are defined by self.add_sweep calls.
+    body(self):
+        Defines the structure of the actual measurement and will be looped over reps times.
+    """
     def initialize(self):
-        cfg=self.cfg
+        """
+        Initialization of the qick program, and configuration of the ND-sweeps is performed in this method.
+        """
 
-        #Qubit refers to the probe pulse
-        #Res refers to readout
-        #Cavity refers to the low power resonant probe
+        cfg = self.cfg
 
-        #Defining local variables for qubit probing and readout.
-        #The channel for the qubit probing is the channel corresponding
-        #to the default pulse variables (pulse_lenght, pulse_phase, etc.)
-        qubit_ch = cfg["qubit_ch"]
-        probe_freq = self.freq2reg(cfg["start"], gen_ch=qubit_ch, ro_ch=cfg["ro_ch"])
-        probe_phase = self.deg2reg(cfg["qubit_phase"], gen_ch=qubit_ch)
-        probe_gain = cfg["qubit_gain"]
-        probe_length = self.us2cycles(cfg['qubit_length'], gen_ch=qubit_ch)
-        cavity_pulse_length = self.us2cycles(cfg['readout_length'], gen_ch=cfg["cavity_ch"])
-        self.declare_gen(ch=cfg["cavity_ch"], nqz=cfg["cavity_nqz"]) #Cavity resonant probe
-        self.declare_gen(ch=qubit_ch, nqz=cfg["qubit_nqz"]) #Qubit probe
+        #defining local variables.
+        probe_ch = cfg["probe_ch"]
+        freq = self.freq2reg(cfg["probe_freq"], gen_ch=probe_ch, ro_ch=cfg["ro_ch"])
+        phase = self.deg2reg(cfg["probe_phase"], gen_ch=probe_ch)
+        gain = cfg["probe_gain"]
+        length = self.us2cycles(cfg['probe_length'], gen_ch=self.cfg['probe_ch'])
+        sweep_variables = cfg["sweep_variables"]
 
-        self.declare_readout(ch=cfg["ro_ch"], length=self.us2cycles(cfg["readout_length"]),
-                             freq=cfg["cavity_freq"], gen_ch=cfg["cavity_ch"])
+        #Declare signal generators and readout
+        self.declare_gen(ch=cfg["probe_ch"], nqz=cfg["nqz"], ro_ch=cfg["ro_ch"])
+        self.declare_readout(ch=cfg["ro_ch"], length=self.us2cycles(self.cfg['readout_length'], ro_ch = self.cfg['ro_ch']),
+                             freq=self.cfg["probe_freq"], gen_ch=cfg["probe_ch"])
 
-        #Find register pages
-        self.q_rp=self.ch_page(qubit_ch)
-        self.r_freq=self.sreg(qubit_ch, "freq")
+        self.set_pulse_registers(ch=probe_ch, style="const", freq=freq, phase=phase, gain=gain, length=length)
 
-        # Get dac register value for cavity frequecy
-        cavity_freq=self.freq2reg(cfg["cavity_freq"], gen_ch=cfg["cavity_ch"], ro_ch=cfg["ro_ch"])
+        for sweep_variable in sweep_variables:
+            if sweep_variable == "probe_length":
+                
+                #Getting the gen manager for calculating the correct start and end points of the mode register.
+                #Thus, by utilizing these methods you may ensure that you will not sent an improper mode register.
+                gen_manager = FullSpeedGenManager(self, cfg["probe_ch"]) 
+                sweep_settings = sweep_variables[sweep_variable]
+                start_length = self.us2cycles(sweep_settings[0])
+                end_length = self.us2cycles(sweep_settings[1])
+                start_code = gen_manager.get_mode_code(length=start_length, outsel="dds")
+                end_code = gen_manager.get_mode_code(length=end_length, outsel="dds")
 
-        #Get start and step size frequencies
-        self.f_start = self.freq2reg(cfg["start"], gen_ch=cfg["qubit_ch"])
-        self.f_step = self.freq2reg(cfg["step"], gen_ch=cfg["qubit_ch"])
+                #The register containing the pulse length as the last 16 bits is referred to as the "mode" register.
+                sweep_register = self.get_gen_reg(cfg["probe_ch"], "mode")
+                self.add_sweep(QickSweep(self, sweep_register, start_code, end_code, sweep_settings[2]))
+            else:
+                sweep_settings = sweep_variables[sweep_variable]
+                sweep_register = self.get_gen_reg(cfg["probe_ch"], sweep_variable.replace('probe_', ''))
+                self.add_sweep(QickSweep(self, sweep_register, sweep_settings[0], sweep_settings[1], sweep_settings[2]))
 
 
-        # add qubit and readout pulses to respective channels
-        self.set_pulse_registers(ch=qubit_ch, style="const", freq=self.f_start, phase=probe_phase, gain=probe_gain,
-                                 length=probe_length)
-
-        self.set_pulse_registers(ch=cfg["cavity_ch"], style="const", freq=cavity_freq, phase=cfg["cavity_phase"], gain=cfg["cavity_gain"],
-                                 length=cavity_pulse_length)
-
-        self.sync_all(self.us2cycles(200))
+        self.synci(200)  #Give processor some time to configure pulses
 
     def body(self):
-        self.pulse(ch=self.cfg["qubit_ch"])  #play probe pulse
-        self.sync_all(self.us2cycles(0.05)) # align channels and wait 50ns
+        """
+            The main structure of the measurement is just the measurement,
+            but the add_sweep commands in the initialize method add inner loops
+            into the qick program instructions.
+        """
 
-        #trigger measurement, play measurement pulse, wait for qubit to relax
-        self.measure(pulse_ch=self.cfg["cavity_ch"],
-             adcs=[0],
-             adc_trig_offset=self.cfg["adc_trig_offset"],
-             wait=True,
-             syncdelay=self.us2cycles(self.cfg["relax_delay"]))
-
-    def update(self):
-        self.mathi(self.q_rp, self.r_freq, self.r_freq, '+', self.f_step) # update frequency list index
+        self.measure(pulse_ch=self.cfg["probe_ch"],
+                     adcs=[self.cfg["ro_ch"]],
+                     pins=[0],
+                     adc_trig_offset=self.cfg["adc_trig_offset"],
+                     wait=True,
+                     syncdelay=self.us2cycles(self.cfg["relax_delay"]))
 
