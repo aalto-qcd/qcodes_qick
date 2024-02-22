e3268f28659cb5f2a2c9a0a37893a7fed6362f82
yoshi74ls181 <yoshi74ls181@gmail.com>
2024-02-22 17:31:08 +0200

    Declare parameters as attributes

--- qcodes_qick/QICKChannel.py
+++ qcodes_qick/QICKChannel.py
@@ -25,52 +25,52 @@ class DACChannel(InstrumentBase):
             "pulse_length": 10,
         }  # -- us
 
-        self.add_parameter(
-            "channel",
-            parameter_class=ManualParameter,
+        self.channel = ManualParameter(
+            name="channel",
+            instrument=self,
             label="Channel number",
             vals=Ints(*[0, 6]),
             initial_value=channel_number,
         )
 
-        self.add_parameter(
-            "nqz",
-            parameter_class=ManualParameter,
+        self.nqz = ManualParameter(
+            name="nqz",
+            instrument=self,
             label="Nyquist zone",
             vals=Ints(1, 2),
             initial_value=1,
         )
 
-        self.add_parameter(
-            "pulse_gain",
-            parameter_class=ManualParameter,
+        self.pulse_gain = ManualParameter(
+            name="pulse_gain",
+            instrument=self,
             label="DAC gain",
             vals=Numbers(*[0, 40000]),
             unit="DAC units",
             initial_value=5000,
         )
 
-        self.add_parameter(
-            "pulse_freq",
-            parameter_class=ManualParameter,
+        self.pulse_freq = ManualParameter(
+            name="pulse_freq",
+            instrument=self,
             label="NCO frequency",
             vals=Numbers(*[0, 9000]),
             unit="MHz",
             initial_value=500,
         )
 
-        self.add_parameter(
-            "pulse_phase",
-            parameter_class=ManualParameter,
+        self.pulse_phase = ManualParameter(
+            name="pulse_phase",
+            instrument=self,
             label="Pulse phase",
             vals=Ints(*[0, 360]),
             unit="deg",
             initial_value=0,
         )
 
-        self.add_parameter(
-            "pulse_length",
-            parameter_class=ManualParameter,
+        self.pulse_length = ManualParameter(
+            name="pulse_length",
+            instrument=self,
             label="Pulse length",
             vals=Numbers(*[0, 150]),
             unit="us",
@@ -96,17 +96,17 @@ class ADCChannel(InstrumentBase):
         self.isDAC = False
         self.isADC = True
 
-        self.add_parameter(
-            "channel",
-            parameter_class=ManualParameter,
+        self.channel = ManualParameter(
+            name="channel",
+            instrument=self,
             label="Channel number",
             vals=Ints(*[0, 1]),
             initial_value=channel_number,
         )
 
-        self.add_parameter(
-            "readout_time",
-            parameter_class=ManualParameter,
+        self.readout_time = ManualParameter(
+            name="readout_time",
+            instrument=self,
             label="Up time of the ADC readout | Used for timetrace applications",
             vals=Numbers(*[0, 1000000]),
             unit="Clock ticks",
