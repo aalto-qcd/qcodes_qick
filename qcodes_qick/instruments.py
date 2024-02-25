ad3cff18beb22634ba814191a0aa5837fcb8241a
yoshi74ls181 <yoshi74ls181@gmail.com>
2024-02-25 19:50:08 +0200

    Add DAC and ADC channels to QickInstrument

--- qcodes_qick/instruments.py
+++ qcodes_qick/instruments.py
@@ -1,4 +1,6 @@
-from qcodes import Instrument
+from qcodes import ChannelTuple, Instrument
+
+from qcodes_qick.channels import QickAdcChannel, QickDacChannel
 from qick.pyro import make_proxy
 
 
@@ -10,4 +12,27 @@ class QickInstrument(Instrument):
         # Use the IP address and port of the Pyro4 nameserver to get:
         #   soc: Pyro4.Proxy pointing to the QickSoc object on the board
         #   soccfg: QickConfig containing the current configuration of the board
-        soc, soccfg = make_proxy(ns_host="10.0.100.16", ns_port=8888)
+        soc, self.soccfg = make_proxy(ns_host="10.0.100.16", ns_port=8888)
+
+        self.dac_count = len(self.soccfg["gens"])
+        self.adc_count = len(self.soccfg["readouts"])
+
+        self.dacs = ChannelTuple(
+            parent=self,
+            name="dacs",
+            chan_type=QickDacChannel,
+            chan_list=[
+                QickDacChannel(self, f"dac{ch}", ch) for ch in range(self.dac_count)
+            ],
+        )
+        self.adcs = ChannelTuple(
+            parent=self,
+            name="adcs",
+            chan_type=QickAdcChannel,
+            chan_list=[
+                QickAdcChannel(self, f"adc{ch}", ch) for ch in range(self.adc_count)
+            ],
+        )
+
+        self.add_submodule("dacs", self.dacs)
+        self.add_submodule("adcs", self.adcs)
