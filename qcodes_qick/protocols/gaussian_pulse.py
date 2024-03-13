5147daa80be40e17a21083b8511bcd4ee5f602ba
yoshi74ls181 <yoshi74ls181@gmail.com>
2024-03-13 22:36:57 +0200

    Refactor out GaussianPulse

--- qcodes_qick/protocols/gaussian_pulse.py
+++ qcodes_qick/protocols/gaussian_pulse.py
@@ -5,15 +5,9 @@ from typing import TYPE_CHECKING, Sequence
 from qcodes import ManualParameter
 from qcodes.validators import Ints
 
-from qcodes_qick.channels import AdcChannel, DacChannel
-from qcodes_qick.parameters import (
-    GainParameter,
-    HzParameter,
-    SecParameter,
-    TProcSecParameter,
-)
 from qcodes_qick.protocol_base import HardwareSweep, SweepProgram, SweepProtocol
-from qick.averager_program import QickSweep
+from qcodes_qick.pulse_base import QickPulse
+from qcodes_qick.pulses.readout import ReadoutPulse
 from qick.qick_asm import QickConfig
 
 if TYPE_CHECKING:
@@ -25,202 +19,35 @@ class GaussianPulseProtocol(SweepProtocol):
     def __init__(
         self,
         parent: QickInstrument,
-        qubit_dac: DacChannel,
-        readout_dac: DacChannel,
-        readout_adc: AdcChannel,
+        qubit_pulse: QickPulse,
+        readout_pulse: ReadoutPulse,
         name="GaussianPulseProtocol",
         **kwargs,
     ):
         super().__init__(parent, name, **kwargs)
-        self.qubit_dac = qubit_dac
-        self.readout_dac = readout_dac
-        self.readout_adc = readout_adc
-        self.readout_dac.matching_adc.set(readout_adc.channel)
-        self.readout_adc.matching_dac.set(readout_dac.channel)
+        self.qubit_pulse = qubit_pulse
+        self.readout_pulse = readout_pulse
+        self.pulses = {qubit_pulse, readout_pulse}
 
-        self.qubit_gain = GainParameter(
-            name="qubit_gain",
-            instrument=self,
-            label="Gain of qubit pulse",
-            initial_value=0.5,
-        )
-
-        self.qubit_freq = HzParameter(
-            name="qubit_freq",
-            instrument=self,
-            label="Frequency of qubit pulse",
-            initial_value=4e9,
-            channel=qubit_dac,
-        )
-
-        self.qubit_sigma = SecParameter(
-            name="qubit_sigma",
-            instrument=self,
-            label="Sigma of the gaussian shape of the qubit pulse",
-            initial_value=25e-9,
-            channel=qubit_dac,
-        )
-
-        self.qubit_length = SecParameter(
-            name="qubit_length",
-            instrument=self,
-            label="Length of qubit pulse",
-            initial_value=100e-9,
-            channel=qubit_dac,
-        )
-
-        self.qubit_count = ManualParameter(
-            name="qubit_count",
+        self.qubit_pulse_count = ManualParameter(
+            name="qubit_pulse_count",
             instrument=self,
             label="Number of qubit pulses",
             vals=Ints(min_value=0),
             initial_value=1,
         )
 
-        self.qubit_readout_gap = TProcSecParameter(
-            name="qubit_readout_gap",
-            instrument=self,
-            label="Gap between qubit pulse and readout pulse",
-            initial_value=50e-9,
-            qick_instrument=self.parent,
-        )
-
-        self.readout_gain = GainParameter(
-            name="readout_gain",
-            instrument=self,
-            label="Gain of readout pulse",
-            initial_value=0.5,
-        )
-
-        self.readout_freq = HzParameter(
-            name="readout_freq",
-            instrument=self,
-            label="Frequency of readout pulse",
-            initial_value=6e9,
-            channel=self.readout_dac,
-        )
-
-        self.readout_length = SecParameter(
-            name="readout_length",
-            instrument=self,
-            label="Length of readout pulse",
-            initial_value=10e-6,
-            channel=self.readout_dac,
-        )
-
-        self.adc_trig_offset = TProcSecParameter(
-            name="adc_trig_offset",
-            instrument=self,
-            label="Delay between sending probe pulse and ADC initialization",
-            initial_value=0,
-            qick_instrument=self.parent,
-        )
-
-        self.relax_delay = TProcSecParameter(
-            name="relax_delay",
-            instrument=self,
-            label="Delay between reps",
-            initial_value=1e-3,
-            qick_instrument=self.parent,
-        )
-
     def generate_program(
         self, soccfg: QickConfig, hardware_sweeps: Sequence[HardwareSweep] = ()
     ):
-        return GaussianPulseProtocol(soccfg, self, hardware_sweeps)
+        return GaussianPulseProgram(soccfg, self, hardware_sweeps)
 
 
 class GaussianPulseProgram(SweepProgram):
 
     protocol: GaussianPulseProtocol
 
-    def initialize(self):
-        self.declare_gen(
-            ch=self.protocol.qubit_dac.channel,
-            nqz=self.protocol.qubit_dac.nqz.get(),
-        )
-        self.declare_gen(
-            ch=self.protocol.readout_dac.channel,
-            nqz=self.protocol.readout_dac.nqz.get(),
-        )
-        self.declare_readout(
-            ch=self.protocol.readout_adc.channel,
-            length=self.protocol.readout_length.get_raw(),
-            sel="product",
-            freq=self.protocol.readout_freq.get() / 1e6,
-        )
-        self.add_gauss(
-            ch=self.protocol.qubit_dac.channel,
-            name="qubit",
-            sigma=self.protocol.qubit_sigma.get_raw(),
-            length=self.protocol.qubit_length.get_raw(),
-        )
-        self.set_pulse_registers(
-            ch=self.protocol.qubit_dac.channel,
-            style="arb",
-            freq=self.protocol.qubit_freq.get_raw(),
-            phase=0,
-            gain=self.protocol.qubit_gain.get_raw(),
-            phrst=0,
-            stdysel="zero",
-            mode="oneshot",
-            waveform="qubit",
-        )
-        self.set_pulse_registers(
-            ch=self.protocol.readout_dac.channel,
-            style="const",
-            freq=self.protocol.readout_freq.get_raw(),
-            phase=0,
-            gain=self.protocol.readout_gain.get_raw(),
-            phrst=0,
-            stdysel="zero",
-            mode="oneshot",
-            length=self.protocol.readout_length.get_raw(),
-        )
-        self.qubit_readout_gap_reg = self.new_gen_reg(
-            gen_ch=self.protocol.readout_dac.channel,
-            name="qubit_readout_gap_reg",
-            init_val=self.protocol.qubit_readout_gap.get() * 1e6,
-            reg_type="time",
-            tproc_reg=True,
-        )
-
-        for sweep in reversed(self.hardware_sweeps):
-            if sweep.parameter is self.protocol.qubit_gain:
-                reg = self.get_gen_reg(self.protocol.qubit_dac.channel, "gain")
-                self.add_sweep(
-                    QickSweep(self, reg, sweep.start_int, sweep.stop_int, sweep.num)
-                )
-            elif sweep.parameter is self.protocol.qubit_freq:
-                reg = self.get_gen_reg(self.protocol.qubit_dac.channel, "freq")
-                self.add_sweep(
-                    QickSweep(self, reg, sweep.start / 1e6, sweep.stop / 1e6, sweep.num)
-                )
-            elif sweep.parameter is self.protocol.qubit_readout_gap:
-                reg = self.qubit_readout_gap_reg
-                self.add_sweep(
-                    QickSweep(self, reg, sweep.start * 1e6, sweep.stop * 1e6, sweep.num)
-                )
-            elif sweep.parameter is self.protocol.readout_gain:
-                reg = self.get_gen_reg(self.protocol.readout_dac.channel, "gain")
-                self.add_sweep(
-                    QickSweep(self, reg, sweep.start_int, sweep.stop_int, sweep.num)
-                )
-            else:
-                raise NotImplementedError
-
-        self.synci(200)  # Give processor some time to configure pulses
-
     def body(self):
-        for _ in range(self.protocol.qubit_count.get()):
-            self.pulse(ch=self.protocol.qubit_dac.channel, t="auto")
-        self.sync_all()
-        self.sync(self.qubit_readout_gap_reg.page, self.qubit_readout_gap_reg.addr)
-        self.measure(
-            adcs=[self.protocol.readout_adc.channel],
-            pulse_ch=self.protocol.readout_dac.channel,
-            adc_trig_offset=self.protocol.adc_trig_offset.get_raw(),
-            t="auto",
-            wait=True,
-            syncdelay=self.protocol.relax_delay.get_raw(),
-        )
+        for _ in range(self.protocol.qubit_pulse_count.get()):
+            self.protocol.qubit_pulse.play(self)
+        self.protocol.readout_pulse.play(self, wait_for_adc=True)
