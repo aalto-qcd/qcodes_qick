870368fb489d07338a1dfd42b35103f7158335dc
yoshi74ls181 <yoshi74ls181@gmail.com>
2024-03-13 18:31:39 +0200

    Create SweepProgram

--- qcodes_qick/protocols/ramsey.py
+++ qcodes_qick/protocols/ramsey.py
@@ -10,15 +10,15 @@ from qcodes_qick.parameters import (
     SecParameter,
     TProcSecParameter,
 )
-from qcodes_qick.protocol_base import HardwareSweep, NDAveragerProtocol
-from qick.averager_program import NDAveragerProgram, QickSweep
+from qcodes_qick.protocol_base import HardwareSweep, SweepProgram, SweepProtocol
+from qick.averager_program import QickSweep
 from qick.qick_asm import QickConfig
 
 if TYPE_CHECKING:
     from qcodes_qick.instruments import QickInstrument
 
 
-class RamseyProtocol(NDAveragerProtocol):
+class RamseyProtocol(SweepProtocol):
 
     def __init__(
         self,
@@ -130,94 +130,94 @@ class RamseyProtocol(NDAveragerProtocol):
             qick_instrument=self.parent,
         )
 
-    def generate_program(self, soccfg: QickConfig, cfg: dict) -> RamseyProgram:
-        return RamseyProgram(soccfg, cfg)
+    def generate_program(
+        self, soccfg: QickConfig, hardware_sweeps: Sequence[HardwareSweep] = ()
+    ):
+        return RamseyProgram(soccfg, self, hardware_sweeps)
 
 
+class RamseyProgram(SweepProgram):
 
-class RamseyProgram(NDAveragerProgram):
+    protocol: RamseyProtocol
 
     def initialize(self):
-        p: RamseyProtocol = self.cfg["protocol"]
-        hardware_sweeps: Sequence[HardwareSweep] = self.cfg.get("hardware_sweeps", ())
-
         self.declare_gen(
-            ch=p.qubit_dac.channel,
-            nqz=p.qubit_dac.nqz.get(),
+            ch=self.protocol.qubit_dac.channel,
+            nqz=self.protocol.qubit_dac.nqz.get(),
         )
         self.declare_gen(
-            ch=p.readout_dac.channel,
-            nqz=p.readout_dac.nqz.get(),
+            ch=self.protocol.readout_dac.channel,
+            nqz=self.protocol.readout_dac.nqz.get(),
         )
         self.declare_readout(
-            ch=p.readout_adc.channel,
-            length=p.readout_length.get_raw(),
+            ch=self.protocol.readout_adc.channel,
+            length=self.protocol.readout_length.get_raw(),
             sel="product",
-            freq=p.readout_freq.get() / 1e6,
+            freq=self.protocol.readout_freq.get() / 1e6,
         )
         self.add_gauss(
-            ch=p.qubit_dac.channel,
+            ch=self.protocol.qubit_dac.channel,
             name="qubit",
-            sigma=p.qubit_sigma.get_raw(),
-            length=p.qubit_length.get_raw(),
+            sigma=self.protocol.qubit_sigma.get_raw(),
+            length=self.protocol.qubit_length.get_raw(),
         )
         self.set_pulse_registers(
-            ch=p.qubit_dac.channel,
+            ch=self.protocol.qubit_dac.channel,
             style="arb",
-            freq=p.qubit_freq.get_raw(),
+            freq=self.protocol.qubit_freq.get_raw(),
             phase=0,
-            gain=p.half_pi_gain.get_raw(),
+            gain=self.protocol.half_pi_gain.get_raw(),
             phrst=0,
             stdysel="zero",
             mode="oneshot",
             waveform="qubit",
         )
         self.qubit_relative_phase_reg = self.new_gen_reg(
-            gen_ch=p.qubit_dac.channel,
+            gen_ch=self.protocol.qubit_dac.channel,
             name="qubit_relative_phase",
-            init_val=p.qubit_relative_phase.get(),
+            init_val=self.protocol.qubit_relative_phase.get(),
             reg_type="phase",
         )
         self.set_pulse_registers(
-            ch=p.readout_dac.channel,
+            ch=self.protocol.readout_dac.channel,
             style="const",
-            freq=p.readout_freq.get_raw(),
+            freq=self.protocol.readout_freq.get_raw(),
             phase=0,
-            gain=p.readout_gain.get_raw(),
+            gain=self.protocol.readout_gain.get_raw(),
             phrst=0,
             stdysel="zero",
             mode="oneshot",
-            length=p.readout_length.get_raw(),
+            length=self.protocol.readout_length.get_raw(),
         )
         self.qubit_qubit_gap_reg = self.new_gen_reg(
-            gen_ch=p.qubit_dac.channel,
+            gen_ch=self.protocol.qubit_dac.channel,
             name="qubit_qubit_gap",
-            init_val=p.qubit_qubit_gap.get() * 1e6,
+            init_val=self.protocol.qubit_qubit_gap.get() * 1e6,
             reg_type="time",
             tproc_reg=True,
         )
 
-        for sweep in reversed(hardware_sweeps):
-            if sweep.parameter is p.half_pi_gain:
-                reg = self.get_gen_reg(p.qubit_dac.channel, "gain")
+        for sweep in reversed(self.hardware_sweeps):
+            if sweep.parameter is self.protocol.half_pi_gain:
+                reg = self.get_gen_reg(self.protocol.qubit_dac.channel, "gain")
                 self.add_sweep(
                     QickSweep(self, reg, sweep.start_int, sweep.stop_int, sweep.num)
                 )
-            elif sweep.parameter is p.qubit_freq:
-                reg = self.get_gen_reg(p.qubit_dac.channel, "freq")
+            elif sweep.parameter is self.protocol.qubit_freq:
+                reg = self.get_gen_reg(self.protocol.qubit_dac.channel, "freq")
                 self.add_sweep(
                     QickSweep(self, reg, sweep.start / 1e6, sweep.stop / 1e6, sweep.num)
                 )
-            elif sweep.parameter is p.qubit_relative_phase:
+            elif sweep.parameter is self.protocol.qubit_relative_phase:
                 reg = self.qubit_relative_phase_reg
                 self.add_sweep(QickSweep(self, reg, sweep.start, sweep.stop, sweep.num))
-            elif sweep.parameter is p.qubit_qubit_gap:
+            elif sweep.parameter is self.protocol.qubit_qubit_gap:
                 reg = self.qubit_qubit_gap_reg
                 self.add_sweep(
                     QickSweep(self, reg, sweep.start * 1e6, sweep.stop * 1e6, sweep.num)
                 )
-            elif sweep.parameter is p.readout_gain:
-                reg = self.get_gen_reg(p.readout_dac.channel, "gain")
+            elif sweep.parameter is self.protocol.readout_gain:
+                reg = self.get_gen_reg(self.protocol.readout_dac.channel, "gain")
                 self.add_sweep(
                     QickSweep(self, reg, sweep.start_int, sweep.stop_int, sweep.num)
                 )
@@ -227,21 +227,20 @@ class RamseyProgram(NDAveragerProgram):
         self.synci(200)  # Give processor some time to configure pulses
 
     def body(self):
-        p: RamseyProtocol = self.cfg["protocol"]
-        qubit_phase_reg = self.get_gen_reg(p.qubit_dac.channel, "phase")
+        qubit_phase_reg = self.get_gen_reg(self.protocol.qubit_dac.channel, "phase")
 
         qubit_phase_reg.set_to(0)
-        self.pulse(ch=p.qubit_dac.channel, t="auto")
+        self.pulse(ch=self.protocol.qubit_dac.channel, t="auto")
         self.sync_all()
         self.sync(self.qubit_qubit_gap_reg.page, self.qubit_qubit_gap_reg.addr)
         qubit_phase_reg.set_to(self.qubit_relative_phase_reg)
-        self.pulse(ch=p.qubit_dac.channel, t="auto")
-        self.sync_all(t=p.qubit_readout_gap.get_raw())
+        self.pulse(ch=self.protocol.qubit_dac.channel, t="auto")
+        self.sync_all(t=self.protocol.qubit_readout_gap.get_raw())
         self.measure(
-            adcs=[p.readout_adc.channel],
-            pulse_ch=p.readout_dac.channel,
-            adc_trig_offset=p.adc_trig_offset.get_raw(),
+            adcs=[self.protocol.readout_adc.channel],
+            pulse_ch=self.protocol.readout_dac.channel,
+            adc_trig_offset=self.protocol.adc_trig_offset.get_raw(),
             t="auto",
             wait=True,
-            syncdelay=p.relax_delay.get_raw(),
+            syncdelay=self.protocol.relax_delay.get_raw(),
         )
