75c7d87f2d4dfb728244c5477aaf43a2239f5f9b
yoshi74ls181 <yoshi74ls181@gmail.com>
2024-03-16 19:13:53 +0200

    Enable `from qcodes_qick.instructions import *` and `from qcodes_qick.protocols import *`

---
+++ qcodes_qick/instructions/__init__.py
@@ -0,0 +1,5 @@
+from qcodes_qick.instructions.constant_pulse import ConstantPulse
+from qcodes_qick.instructions.delay import Delay
+from qcodes_qick.instructions.gaussian_pulse import GaussianPulse
+from qcodes_qick.instructions.readout_pulse import ReadoutPulse
+from qcodes_qick.instructions.set_phase import SetPhase
---
+++ qcodes_qick/protocols/__init__.py
@@ -0,0 +1,2 @@
+from qcodes_qick.protocols.pulse_probe import PulseProbeProtocol
+from qcodes_qick.protocols.s21 import S21Protocol
