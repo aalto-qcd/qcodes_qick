088e6794af29d5da04e68d314d915393a2102d8f
yoshi74ls181 <yoshi74ls181@gmail.com>
2024-04-04 13:02:11 +0300

    Define __all__ in __init__ to make Ruff happy

--- qcodes_qick/__init__.py
+++ qcodes_qick/__init__.py
@@ -1,2 +1,4 @@
 from qcodes_qick.instruments import QickInstrument
 from qcodes_qick.protocol_base import HardwareSweep, SoftwareSweep
+
+__all__ = [QickInstrument, HardwareSweep, SoftwareSweep]
--- qcodes_qick/instructions/__init__.py
+++ qcodes_qick/instructions/__init__.py
@@ -3,3 +3,11 @@ from qcodes_qick.instructions.delay import Delay
 from qcodes_qick.instructions.gaussian_pulse import GaussianPulse
 from qcodes_qick.instructions.readout_pulse import ReadoutPulse
 from qcodes_qick.instructions.set_phase import SetPhase
+
+__all__ = [
+    ConstantPulse,
+    Delay,
+    GaussianPulse,
+    ReadoutPulse,
+    SetPhase,
+]
