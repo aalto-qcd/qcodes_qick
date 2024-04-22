7ad161419f92c81877f60a5a264cd91f27ae16df
yoshi74ls181 <yoshi74ls181@gmail.com>
2024-04-22 18:14:53 +0300

    Make Ruff happy

--- tests/test_import_all_modules.py
+++ tests/test_import_all_modules.py
@@ -5,7 +5,7 @@ import os
 
 def import_submodules(module_name: str, module_path: str):
     for _, name, _ in pkgutil.iter_modules([module_path]):
-        submodule_name = module_name + '.' + name
+        submodule_name = module_name + "." + name
         submodule_path = os.path.join(module_path, name)
         importlib.import_module(submodule_name)
         if os.path.isdir(submodule_path):
