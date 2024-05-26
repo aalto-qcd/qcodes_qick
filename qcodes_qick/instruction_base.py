b21c3c1bb089b5a9de446c22a0a0c4895b29da71
yoshi74ls181 <yoshi74ls181@gmail.com>
2024-05-26 10:53:43 +0300

    Add docstring

--- qcodes_qick/instruction_base.py
+++ qcodes_qick/instruction_base.py
@@ -14,6 +14,18 @@ class QickInstruction(InstrumentModule):
     parent: QickInstrument
 
     def __init__(self, parent: QickInstrument, name: str, **kwargs: Any):
+        """
+        Parameters
+        ----------
+        parent : QickInstrument
+            Make me a submodule of this QickInstrument.
+        adc : AdcChannel
+            The ADC channel to use.
+        name : str
+            My unique name.
+        **kwargs : dict, optional
+            Keyword arguments to pass on to InstrumentBase.__init__.
+        """
         super().__init__(parent, name, **kwargs)
         self.dacs: Sequence[DacChannel] = []
         self.adcs: Sequence[AdcChannel] = []
@@ -21,10 +33,29 @@ class QickInstruction(InstrumentModule):
         parent.add_submodule(name, self)
 
     def initialize(self, program: SweepProgram):
+        """Add initialization commands to a program.
+
+        Parameters
+        ----------
+        program : SweepProgram
+        """
         pass
 
     def play(self, program: SweepProgram):
+        """Append me to a program.
+
+        Parameters
+        ----------
+        program : SweepProgram
+        """
         pass
 
     def add_sweep(self, program: SweepProgram, sweep: HardwareSweep):
+        """Add a sweep over one of my parameters to a program.
+
+        Parameters
+        ----------
+        program : SweepProgram
+        sweep: HardwareSweep
+        """
         raise NotImplementedError(f"cannot sweep over {sweep.parameter.name}")
