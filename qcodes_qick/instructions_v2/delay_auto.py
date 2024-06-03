3d679bd1f72325f4633420a385437da2f77ac55c
yoshi74ls181 <yoshi74ls181@gmail.com>
2024-06-03 18:50:23 +0300

    Add DelayAuto

---
+++ qcodes_qick/instructions_v2/delay_auto.py
@@ -0,0 +1,68 @@
+from qcodes_qick.channels import DacChannel
+from qcodes_qick.instruction_base_v2 import QickInstruction
+from qcodes_qick.instruments import QickInstrument
+from qcodes_qick.parameters import TProcSecParameter
+from qcodes_qick.protocol_base_v2 import HardwareSweep, SweepProgram
+
+
+class DelayAuto(QickInstruction):
+    """Add a delay, counting from the end of the last pulse or readout.
+
+    Parameters
+    ----------
+    parent : QickInstrument
+        Make me a submodule of this QickInstrument.
+    dac : DacChannel
+        The DAC channel to add the delay to.
+    name : str
+        My unique name.
+    **kwargs : dict, optional
+        Keyword arguments to pass on to InstrumentBase.__init__.
+    """
+
+    def __init__(
+        self,
+        parent: QickInstrument,
+        dac: DacChannel,
+        name="Delay",
+        **kwargs,
+    ):
+        super().__init__(parent, dacs=[dac], name=name, **kwargs)
+
+        self.time = TProcSecParameter(
+            name="time",
+            instrument=self,
+            label="Delay time",
+            initial_value=1e-6,
+            qick_instrument=self.parent,
+        )
+
+    def initialize(self, program: SweepProgram):
+        """Add initialization commands to a program.
+
+        Parameters
+        ----------
+        program : SweepProgram
+        """
+
+    def play(self, program: SweepProgram):
+        """Append me to a program.
+
+        Parameters
+        ----------
+        program : SweepProgram
+        """
+        assert self in program.protocol.instructions
+        program.delay_auto(self.time.get() * 1e6)
+
+    def add_sweep(self, program: SweepProgram, sweep: HardwareSweep):
+        """Add a sweep over one of my parameters to a program.
+
+        Parameters
+        ----------
+        program : SweepProgram
+        sweep: HardwareSweep
+        """
+        raise NotImplementedError(
+            f"cannot perform a hardware sweep over {sweep.parameter.name}"
+        )
