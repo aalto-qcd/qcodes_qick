46bca713e0bce725d4bcb359e9244627d2f89689
yoshi74ls181 <yoshi74ls181@gmail.com>
2024-12-03 10:02:36 +0200

    Make Ruff happy

--- qcodes_qick/envelopes_v2/__init__.py
+++ qcodes_qick/envelopes_v2/__init__.py
@@ -2,6 +2,6 @@ from qcodes_qick.envelopes_v2.gaussian import GaussianEnvelope
 from qcodes_qick.envelopes_v2.gaussian_drag import GaussianDragEnvelope
 
 __all__ = [
-    "GaussianEnvelope",
     "GaussianDragEnvelope",
+    "GaussianEnvelope",
 ]
