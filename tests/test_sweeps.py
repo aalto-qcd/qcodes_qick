9e4966fab3dc8063b41e42af9c6046d1ab3faf32
Ashish Panigrahi <public@ashishpanigrahi.com>
2026-05-26 12:00:26 +0300

    simple check for numpy array input to software sweep

--- tests/test_sweeps.py
+++ tests/test_sweeps.py
@@ -11,3 +11,17 @@ import numpy as np
 from qcodes import ManualParameter
 
 from qcodes_qick.protocol_base import HardwareSweep, SoftwareSweep
+
+
+class TestSoftwareSweep(unittest.TestCase):
+    def _param(self, unit: str = "") -> ManualParameter:
+        return ManualParameter("p", unit=unit)
+
+    def test_numpy_linspace_input(self):
+        sweep = SoftwareSweep(self._param(), np.linspace(0, 1, 21))
+        self.assertEqual(len(sweep.values), 21)
+        np.testing.assert_allclose(np.asarray(sweep.values)[[0, -1]], [0, 1])
+
+
+if __name__ == "__main__":
+    unittest.main()
