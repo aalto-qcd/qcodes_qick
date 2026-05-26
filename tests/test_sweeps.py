128db75751042e67e78cf5fd432ff3ec4275d8de
Ashish Panigrahi <public@ashishpanigrahi.com>
2026-05-26 12:42:13 +0300

    removed unused HardwareSweep import for tests

--- tests/test_sweeps.py
+++ tests/test_sweeps.py
@@ -2,7 +2,7 @@
 qcodes_qick.protocol_base.py
 
 These tests run without the hardware. These cover just the logic of
-SoftwareSweep and HardwareSweep, which is purely pythonic.
+SoftwareSweep, which is purely pythonic.
 """
 
 import unittest
@@ -10,7 +10,7 @@ import unittest
 import numpy as np
 from qcodes import ManualParameter
 
-from qcodes_qick.protocol_base import HardwareSweep, SoftwareSweep
+from qcodes_qick.protocol_base import SoftwareSweep
 
 
 class TestSoftwareSweep(unittest.TestCase):
